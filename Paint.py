1b60231bd96531d030a19fd7724f018f90c66a0f
GamingoDino <122396821+Gaming0Dino@users.noreply.github.com>
2023-01-12 17:59:13 +0000

    more paint

--- Paint.py
+++ Paint.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+from tkinter import *
+from tkinter import colorchooser
 
 pygame.init()
 
@@ -61,6 +63,10 @@ class drawing(object):
                                 self.tick += 1
                                 pygame.draw.rect(
                                     win, (255, 255, 255), (710, 342, 80, 35))
+                            if button.action == 5:
+                                color_code = colorchooser.askcolor(title ="Choose color")
+                                color_code = color_code[0]
+                                self.color = color_code
 
         for button in list2:
             if button.action == 4:
@@ -85,7 +91,6 @@ class button(object):
                                         self.width, self.height), self.outline)
         font = pygame.font.SysFont('comicsansms', 30)
         text = font.render(self.text, 1, self.color2)
-        #pygame.draw.rect(win, (255, 255, 255), (710, 456, 80, 35))
         win.blit(text, (int(self.x+self.width/2-text.get_width()/2),
                         int(self.y+self.height/2-text.get_height()/2)))
 
@@ -94,14 +99,14 @@ def drawHeader(win):
     # Drawing header space
     pygame.draw.rect(win, (175, 171, 171), (0, 0, 800, 35))
     pygame.draw.rect(win, (175, 171, 171), (699, 33, 101, 35))
-    pygame.draw.rect(win, (175, 171, 171), (699, 256, 101, 35))
-    pygame.draw.rect(win, (175, 171, 171), (699, 388, 101, 35))
+    pygame.draw.rect(win, (175, 171, 171), (699, 302, 101, 35))
+    pygame.draw.rect(win, (175, 171, 171), (699, 434, 101, 35))
     pygame.draw.rect(win, (0, 0, 0), (0, 0, 700, 35), 2)
     pygame.draw.rect(win, (0, 0, 0), (699, 0, 101, 35), 2)
     pygame.draw.rect(win, (0, 0, 0), (699, 33, 101, 767), 2)
     pygame.draw.rect(win, (0, 0, 0), (699, 33, 101, 35), 2)
-    pygame.draw.rect(win, (0, 0, 0), (699, 256, 101, 35), 2)
-    pygame.draw.rect(win, (0, 0, 0), (699, 388, 101, 35), 2)
+    pygame.draw.rect(win, (0, 0, 0), (699, 302, 101, 35), 2)
+    pygame.draw.rect(win, (0, 0, 0), (699, 434, 101, 35), 2)
 
     # Printing header
     font = pygame.font.SysFont('comicsansms', 30)
@@ -120,11 +125,11 @@ def drawHeader(win):
 
     sizeText = font.render('Size', 1, (0, 0, 0))
     win.blit(sizeText, (int(750 - sizeText.get_width() / 2),
-                        int(541 / 2 - sizeText.get_height() / 2 + 2)))
+                        int(633 / 2 - sizeText.get_height() / 2 + 2)))
 
     toolText = font.render('Tools', 1, (0, 0, 0))
     win.blit(toolText, (int(750 - toolText.get_width() / 2),
-                        int(804 / 2 - toolText.get_height() / 2 + 2)))
+                        int(896 / 2 - toolText.get_height() / 2 + 2)))
 
 def draw(win):
     player1.click(win, Buttons_color, Buttons_other)
@@ -182,17 +187,18 @@ yellowButton = button(707, 132+33, 40, 40, (255, 255, 0), (255, 255, 0))
 purpleButton = button(753, 132+33, 40, 40, (112, 48, 160), (112, 48, 160))
 blackButton = button(707, 178+33, 40, 40, (0, 0, 0), (0, 0, 0))
 whiteButton = button(753, 178+33, 40, 40, (0, 0, 0), (255, 255, 255), 1)
+moreDisplay = button(707, 224+33, 86, 40, (201, 201, 201), (0, 0, 0), 0, 5, 'More')
 
-smallerButton = button(707, 224+72, 40, 40, (201, 201, 201), (0, 0, 0), 0, 2, '-')
-biggerButton = button(753, 224+72, 40, 40, (201, 201, 201), (0, 0, 0), 0, 3, '+')
-sizeDisplay = button(707, 270+72, 86, 40, (0, 0, 0), (0, 0, 0), 1, 4, 'Size')
+smallerButton = button(707, 270+72, 40, 40, (201, 201, 201), (0, 0, 0), 0, 2, '-')
+biggerButton = button(753, 270+72, 40, 40, (201, 201, 201), (0, 0, 0), 0, 3, '+')
+sizeDisplay = button(707, 316+72, 86, 40, (0, 0, 0), (0, 0, 0), 1, 4, 'Size')
 
 # Defining other buttons
-clrButton = button(707, 316+113, 86, 40, (201, 201, 201), (0, 0, 0), 0, 1, 'Clear')
+clrButton = button(707, 362+113, 86, 40, (201, 201, 201), (0, 0, 0), 0, 1, 'Clear')
 
 Buttons_color = [blueButton, redButton, greenButton, orangeButton,
                 yellowButton, purpleButton, blackButton, whiteButton]
 Buttons_other = [clrButton, smallerButton, biggerButton,
-                sizeDisplay]
+                sizeDisplay, moreDisplay]
 
 main_loop()
